fd6e2a2e33be382bb3bb14da5a5411e37710798a
Spigushe <martin.cuchet@gmail.com>
2024-01-21 20:40:49 +0100

    update

--- decklists_after_player.py
+++ decklists_after_player.py
@@ -1,17 +1,16 @@
-import json
 from datetime import datetime
 
 from mtgdc_aggregator import Aggregator
 from mtgdc_decklists import ImportDecks
 
-players = ["spigushe", "zohoz", "domino", "mars"]
-
 if __name__ == "__main__":
+    players = ["spigushe", "zohoz", "domino", "mars"]
+
     for player in players:
         decks = ImportDecks.from_directory()
         decks.load_decks(date_from=datetime(2022, 9, 1), player=player)
-        decks.palmares(f"output/{player}_palmares.txt")
+        decks.palmares(f"output/{player}_palmares.txt", use_url=True)
 
-        aggregation = Aggregator(decks.decklists, ordre=3)
+        aggregation = Aggregator(decks.decklists, ordre=3, use_banlist=False)
         aggregation.aggregate()
         aggregation.export(f"output/{player}_aggregation.txt")
